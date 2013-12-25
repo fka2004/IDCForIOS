48f9c46d2c852ef0f958df8dbfbda0ceed4326fc
fka2004 <zxcfka2004@gmail.com>
2013-12-26 01:12:26 +0800

    Initial Commit

---
+++ IDCForIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IDCForIOS
+//
+//  Created by Mac on 13-12-26.
+//  Copyright (c) 2013年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IDCForIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDCForIOS
+//
+//  Created by Mac on 13-12-26.
+//  Copyright (c) 2013年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
